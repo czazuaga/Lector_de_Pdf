1eaf9c95198d8d9500503118762acc0fd9c699bd
Carlos Zamora <czazuaga@gmail.com>
2017-02-05 14:55:50 +0100

    Pdf API Implementation

---
+++ app/src/main/java/com/digitalmango/lector_pdf/MainActivity_.java
@@ -0,0 +1,181 @@
+package com.digitalmango.lector_pdf;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.OpenableColumns;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+import com.shockwave.pdfium.PdfDocument;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.NonConfigurationInstance;
+import org.androidannotations.annotations.OnActivityResult;
+import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.ViewById;
+
+import java.util.List;
+
+@EActivity(R.layout.activity_main)
+@OptionsMenu(R.menu.options)
+public class MainActivity_ extends AppCompatActivity implements OnPageChangeListener, OnLoadCompleteListener {
+
+    private static final String TAG = MainActivity_.class.getSimpleName();
+    private final static int REQUEST_CODE = 42;
+    public static final int PERMISSION_CODE = 42042;
+    public static final String SAMPLE_FILE = "sample.pdf";
+    public static final String READ_EXTERNAL_STORAGE = "android.permission.READ_EXTERNAL_STORAGE";
+
+    @ViewById PDFView pdfView;
+
+
+    @NonConfigurationInstance Uri uri;
+
+
+    @NonConfigurationInstance Integer pageNumber = 0;
+
+
+    String pdfFileName;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf);
+
+        //pdfView = (PDFView) findViewById(R.id.pdfView);
+
+        //pdfView.setBackgroundColor(getResources().getColor(R.color.colorDivider));
+
+        launchPicker();
+
+    }
+
+
+
+    // Métodos de la interfaz para la gestión de los archivos Pdf ---------------
+
+    @Override
+    public void loadComplete(int nbPages) {
+        PdfDocument.Meta meta = pdfView.getDocumentMeta();
+        Log.e(TAG, "title = " + meta.getTitle());
+        Log.e(TAG, "author = " + meta.getAuthor());
+        Log.e(TAG, "subject = " + meta.getSubject());
+        Log.e(TAG, "keywords = " + meta.getKeywords());
+        Log.e(TAG, "creator = " + meta.getCreator());
+        Log.e(TAG, "producer = " + meta.getProducer());
+        Log.e(TAG, "creationDate = " + meta.getCreationDate());
+        Log.e(TAG, "modDate = " + meta.getModDate());
+
+        printBookmarksTree(pdfView.getTableOfContents(), "-");
+    }
+
+    @Override
+    public void onPageChanged(int page, int pageCount) {
+        pageNumber = page;
+        setTitle(String.format("%s %s / %s", pdfFileName, page + 1, pageCount));
+    }
+
+    @AfterViews
+    void afterViews() {
+        if (uri != null) {
+            displayFromUri(uri);
+        } else {
+            displayFromAsset(SAMPLE_FILE);
+        }
+        setTitle(pdfFileName);
+    }
+
+
+    // Gestión de archivos ---------------------------------
+
+    void launchPicker() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("application/pdf");
+        try {
+            startActivityForResult(intent, REQUEST_CODE);
+        } catch (ActivityNotFoundException e) {
+            //alert user that file manager not working
+            Toast.makeText(this, R.string.toast_pick_file_error, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+        }
+        if (result == null) {
+            result = uri.getLastPathSegment();
+        }
+        return result;
+    }
+
+    //Gestión del retorno del resultado de la selección del archivo
+
+    @OnActivityResult(REQUEST_CODE)
+    public void onResult(int resultCode, Intent intent) {
+        if (resultCode == RESULT_OK) {
+            uri = intent.getData();
+            displayFromUri(uri);
+        }
+    }
+
+    // Métodos para mostrar el pdf y su información ----------------------------------
+
+    private void displayFromUri(Uri uri) {
+        pdfFileName = getFileName(uri);
+
+        pdfView.fromUri(uri)
+                .defaultPage(pageNumber)
+                .onPageChange(this)
+                .enableAnnotationRendering(true)
+                .onLoad(this)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .load();
+    }
+
+    private void displayFromAsset(String assetFileName) {
+        pdfFileName = assetFileName;
+
+        pdfView.fromAsset(SAMPLE_FILE)
+                .defaultPage(pageNumber)
+                .onPageChange(this)
+                .enableAnnotationRendering(true)
+                .onLoad(this)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .load();
+    }
+
+    private void printBookmarksTree(List<PdfDocument.Bookmark> tableOfContents, String s) {
+        for (PdfDocument.Bookmark b : tableOfContents) {
+
+            Log.e(TAG, String.format("%s %s, p %d", s, b.getTitle(), b.getPageIdx()));
+
+            if (b.hasChildren()) {
+                printBookmarksTree(b.getChildren(), s + "-");
+            }
+        }
+    }
+
+
+
+}
